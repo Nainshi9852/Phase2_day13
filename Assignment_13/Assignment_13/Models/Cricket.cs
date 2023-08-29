ee7b797d85938388312cc4651d07a99ae2321499
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-08-29 17:42:03 +0530

    Inital Commit

---
+++ Assignment_13/Assignment_13/Models/Cricket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_13.Models
+{
+    [Table("Cricket")]
+    public class Cricket
+    {
+        [Key]
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int NoWc { get; set; }
+    }
+}
